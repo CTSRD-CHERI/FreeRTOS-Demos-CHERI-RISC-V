399abc9c5050ac2623bc5bb79f8a2b54b5217cb7
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2020-05-13 05:52:36 +0100

    Demo/riscv_generic: Copy get mtime functions from Galois-P1

--- main.c
+++ main.c
@@ -65,6 +65,47 @@ void vApplicationTickHook(void);
 
 void vToggleLED(void);
 
+#if __riscv_xlen == 64
+#define read_csr(reg) ({ unsigned long __tmp; \
+  asm volatile ("csrr %0, " #reg : "=r"(__tmp)); \
+  __tmp; })
+#endif
+
+/**
+ * Capture the current 64-bit cycle count.
+ */
+uint64_t get_cycle_count(void)
+{
+#if __riscv_xlen == 64
+	return read_csr(mcycle);
+#else
+	uint32_t cycle_lo, cycle_hi;
+	asm volatile(
+		"%=:\n\t"
+		"csrr %1, mcycleh\n\t"
+		"csrr %0, mcycle\n\t"
+		"csrr t1, mcycleh\n\t"
+		"bne  %1, t1, %=b"
+		: "=r"(cycle_lo), "=r"(cycle_hi)
+		: // No inputs.
+		: "t1");
+	return (((uint64_t)cycle_hi) << 32) | (uint64_t)cycle_lo;
+#endif
+}
+
+/**
+ * Use `mcycle` counter to get usec resolution.
+ * On RV32 only, reads of the mcycle CSR return the low 32 bits,
+ * while reads of the mcycleh CSR return bits 63–32 of the corresponding
+ * counter.
+ * We convert the 64-bit read into usec. The counter overflows in roughly an hour
+ * and 20 minutes. Probably not a big issue though.
+ * At 50HMz clock rate, 1 us = 50 ticks
+ */
+uint32_t port_get_current_mtime(void)
+{
+	return (uint32_t)(get_cycle_count() / (configCPU_CLOCK_HZ / 1000000));
+}
 /*-----------------------------------------------------------*/
 
 int main(void) {
