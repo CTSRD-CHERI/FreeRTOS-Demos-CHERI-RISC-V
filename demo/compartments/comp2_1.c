2146262bed3a9875c213b8fbb597bea2524c64f2
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2020-08-18 15:01:17 +0100

    RISC-V_Generic/compartments: Add another file comp2_1.c to comp2

--- demo/compartments/comp2_1.c
+++ demo/compartments/comp2_1.c
@@ -37,11 +37,9 @@
 #include "task.h"
 #include "queue.h"
 
-void vComp2(void);
 void vComp2_1(void);
 
-void vComp2(void) {
-  printf("Hello World from Comp2\n");
-  vComp2_1();
+void vComp2_1(void) {
+  printf("Hello World from Comp2_1\n");
 }
 /*-----------------------------------------------------------*/
