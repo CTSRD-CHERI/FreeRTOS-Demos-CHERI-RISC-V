dde5395d0fd16b0156ea974953f62b518d72a7cb
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2020-08-20 14:43:03 +0100

    RISC-V_Generic/rtl: Add rtl-alloc-lock supporting backend

---
+++ demo/compartments/libdl/rtl-alloc-lock.c
@@ -0,0 +1,25 @@
+#include <FreeRTOS.h>
+#include "semphr.h"
+#include "task.h"
+
+static SemaphoreHandle_t _RTEMS_Allocator_Mutex = NULL;
+
+void _RTEMS_Lock_allocator( void )
+{
+  if(_RTEMS_Allocator_Mutex == NULL) {
+    _RTEMS_Allocator_Mutex = xSemaphoreCreateRecursiveMutex();
+    configASSERT(_RTEMS_Allocator_Mutex != NULL);
+  }
+
+  xSemaphoreTakeRecursive( _RTEMS_Allocator_Mutex );
+}
+
+void _RTEMS_Unlock_allocator( void )
+{
+  xSemaphoreGiveRecursive( _RTEMS_Allocator_Mutex );
+}
+
+bool _RTEMS_Allocator_is_owner( void )
+{
+  return xSemaphoreGetMutexHolder(_RTEMS_Allocator_Mutex) == xTaskGetCurrentTaskHandle();
+}
---
+++ demo/compartments/libdl/rtl-alloc-lock.h
@@ -0,0 +1,3 @@
+void _RTEMS_Lock_allocator( void );
+void _RTEMS_Unlock_allocator( void );
+bool _RTEMS_Allocator_is_owner( void );
