244f4348103549cd87005b38cbaff8e96fa087bc
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2020-08-24 08:36:53 +0100

    RISC-V_Generic/libdl: Implement a demo-specific global symbols load of base FreeRTOS

--- demo/compartments/include/rtl/rtl-freertos-compartments.h
+++ demo/compartments/include/rtl/rtl-freertos-compartments.h
@@ -2,6 +2,8 @@
 #define _FREERTOS_RTL_COMPARTMENTS_H_
 
 #include <FreeRTOS.h>
+#include <stdbool.h>
+#include <rtl/rtl-obj.h>
 
 typedef struct compartment {
   void*       cap;
@@ -18,4 +20,5 @@ extern char comp_strtab[configCOMPARTMENTS_NUM][configMAXLEN_COMPNAME];
 int rtl_freertos_compartment_open(const char *name);
 ssize_t rtl_freertos_compartment_read(int fd, void *buffer, UBaseType_t offset, size_t count);
 size_t rtl_freertos_compartment_getsize(int fd);
+size_t rtl_freertos_global_symbols_add(rtems_rtl_obj* obj);
 #endif
--- demo/compartments/libdl/rtl-freertos-compartments.c
+++ demo/compartments/libdl/rtl-freertos-compartments.c
@@ -3,9 +3,12 @@
 #include <string.h>
 #include <rtl/rtl-freertos-compartments.h>
 #include <rtl/rtl-allocator.h>
+#include <rtl/rtl-obj.h>
+#include <rtl/rtl-trace.h>
 #include <errno.h>
 
 #include <FreeRTOS.h>
+#include <elf.h>
 
 compartment_t comp_list[configCOMPARTMENTS_NUM];
 
@@ -56,4 +59,55 @@ ssize_t rtl_freertos_compartment_read(int fd, void *buffer, UBaseType_t offset, 
 
 size_t rtl_freertos_compartment_getsize(int fd) {
   return comp_list[fd].size;
+}
+
+size_t rtl_freertos_global_symbols_add(rtems_rtl_obj* obj) {
+  extern Elf64_Sym  __symtab_start[];
+  extern Elf64_Sym  __symtab_end[];
+  extern char  __strtab_start[];
+  uint32_t syms_count =  ((size_t) __symtab_end - (size_t) __symtab_start) / sizeof(Elf64_Sym);
+
+  rtems_rtl_symbols* symbols;
+  rtems_rtl_obj_sym* sym;
+  uint32_t globals_count = 0;
+
+  for(int i = 0; i < syms_count; i++) {
+    if (((((uint32_t) (__symtab_start[i]).st_info)) >> 4) == STB_GLOBAL) {
+      globals_count++;
+    }
+  }
+
+  if (rtems_rtl_trace (RTEMS_RTL_TRACE_GLOBAL_SYM))
+    printf ("rtl: global symbol add: %zi\n", globals_count);
+
+  obj->global_size = globals_count * sizeof (rtems_rtl_obj_sym);
+  obj->global_table = rtems_rtl_alloc_new (RTEMS_RTL_ALLOC_SYMBOL,
+                                           obj->global_size, true);
+  if (!obj->global_table)
+  {
+    obj->global_size = 0;
+    rtems_rtl_set_error (ENOMEM, "no memory for global symbols");
+    return false;
+  }
+
+  symbols = rtems_rtl_global_symbols ();
+
+  sym = obj->global_table;
+
+  for(int i = 0; i < syms_count; i++) {
+    if (((((uint32_t) (__symtab_start[i]).st_info)) >> 4) == STB_GLOBAL) {
+      sym->name =  &__strtab_start[__symtab_start[i].st_name];
+      sym->value = __symtab_start[i].st_value;
+
+      if (rtems_rtl_trace (RTEMS_RTL_TRACE_GLOBAL_SYM))
+      if (rtems_rtl_symbol_global_find (sym->name) == NULL) {
+        rtems_rtl_symbol_global_insert (symbols, sym);
+        ++sym;
+      }
+    }
+  }
+
+  obj->global_syms = globals_count;
+
+  return globals_count;
 }
