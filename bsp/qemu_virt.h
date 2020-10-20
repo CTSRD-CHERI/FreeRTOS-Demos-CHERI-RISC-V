4eb3e979fc175136331d70cc0ff53937515759e1
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2020-10-20 10:39:49 +0200

    RISC-V-Generic/qemu: Add VIRTIO/NET/PLIC defines

--- bsp/qemu_virt.h
+++ bsp/qemu_virt.h
@@ -7,6 +7,13 @@
 #define configUART16550_REGSHIFT 1
 
 /**
+ * VIRTIO defines
+ */
+#define QEMU_VIRT_NET_MMIO_ADDRESS         0x10008000
+#define QEMU_VIRT_NET_PLIC_INTERRUPT_ID    0x8
+#define QEMU_VIRT_NET_PLIC_INTERRUPT_PRIO  0x1
+
+/**
  * PLIC defines
  */
 #define PLIC_BASE_ADDR (0xC000000ULL)
